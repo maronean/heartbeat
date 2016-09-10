72e07b4c66d1d08b94f2c51cb7750f662e435fcc
maronean <maronean@gmail.com>
2016-09-10 17:27:06 -0400

    added comment
    added comment

--- heartbeat.py
+++ heartbeat.py
@@ -37,6 +37,10 @@ class CriticalProcess(Process):
                 stillGoing = False
                 
 def monitorHB(connRecv):
+"""
+Process to recieve heartbeat from pipe connection
+prints error and terminates upon failure
+"""
     stillGoing = True
     while(stillGoing):
         print('hello')
